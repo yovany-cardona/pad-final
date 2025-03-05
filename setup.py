e65c4b4b24ccb8df0d51a56c948c747c51633eb3
yovany-cardona <yovany.patino@est.iudigital.edu.co>
2025-03-05 03:47:59 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="actividad1",
+    version="0.0.1",
+    author="Yovany Patiño",
+    author_email="",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests"
+    ]
+) 
